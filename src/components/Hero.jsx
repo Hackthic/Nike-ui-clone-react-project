d82e268805518ae8f791c702125c12126f6a004c
Aviral <hackthic@gmail.com>
2024-12-27 00:10:47 +0530

    this is the nike ui clone from the react, this is the basic project made during the learning of the react from chai aur code

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero = () => {
+  return (
+    <div className="maincontent container">
+      <div className="content">
+        <p>YOUR FEET DESERVE THE BEST</p>
+        <h6>
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+        </h6>
+
+        <div className="Buttons">
+          <button>Shop Now</button>
+          <button>Category</button>
+        </div>
+      </div>
+
+      <div className="shoes">
+        <img src="/images/nike.jpg" alt="shoe-image" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
